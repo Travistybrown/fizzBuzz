03d8e61ed281d99ee52c570118521d0f27baed71
Travistybrown <travistbrownsc@gmail.com>
2022-10-11 16:54:50 -0400

    fgfdfgdfdgfdgdfg

--- js/site.js
+++ js/site.js
@@ -1,17 +1,53 @@
-function displayMessage(){
-  
-    let msg =  document.getElementById("message").value;
+function getValues() {
+  // get the value from the page
+  let startValue = document.getElementById("startValue").value;
+  let endValue = document.getElementById("endValue").value;
 
+  // parse the values in to intengers
+  startValue = parseInt(startValue);
+  endValue = parseInt(endValue);
 
-    //alert(msg);
+  if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+    // generate the numbers based on user input
+    let numbers = generateFizzBuzz(startValue, endValue);
 
-    swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg,
-        }
-
-    )
+    // display results on page
+    displayFizzBuzz(numbers);
+  } else {
+    swal.fire({
+      icon: "error",
+      Title: "Oops....",
+      text: "Only integers are allow for Hundo!",
+    });
+  }
 }
 
+function generateFizzBuzz(startValue, endValue) {
+  let numbers = [];
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    numbers.push ("Fizz Buzz");
+  } else if (i % 3 == 0) {
+    numbers.push  ("Fizz");
+  } else if (i % 5 == 0) {
+    numbers.push ("Buzz");
+  } else {
+    numbers.push (i);
+  }
+}
+  
+
+  return numbers;
+}
+
+function displayFizzBuzz(numbers) {
+    let templateRows = "";
+ for ( let i = 0; i < numbers.length; i++){
+    let number = numbers[i];
+    templateRows += `<tr><td>${number}</td></tr>`;
+ }
+
+ 
+
+  document.getElementById("results").innerHTML = templateRows;
+}
